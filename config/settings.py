ff7c0ad7d074867766a9ba3bf0cbfd6b736c569d
作間景 <sakumakei@MacBook-Pro.local>
2022-04-30 18:43:14 +0900

    comment out DEBUG

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ env.read_env(root('.env.dev'))
 # SECRET_KEY = env.str('SECRET_KEY')
  
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG')
+# DEBUG = env.bool('DEBUG')
  
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
  
